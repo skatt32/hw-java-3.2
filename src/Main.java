18e73452a1ff66641fd7a4d19a7dfb04363b7c97
Neto test <test@neto.com>
2023-11-23 17:11:12 +0400

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double heightM = 1.87;
+        int weightKg = 98;
+        double bmi = service.calculate(heightM, weightKg);
+        System.out.println(bmi);
+    }
 }
